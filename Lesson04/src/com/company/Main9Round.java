e485373541f2b1e17ece12ba8f5753689299def3
Salavat Zabirov <zab.sal42@gmail.com>
2019-02-26 18:48:42 +0300

    Lesson04: массивы, задачи на массивах, домашка

---
+++ Lesson04/src/com/company/Main9Round.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main9Round {
+    public static void main(String[] args) {
+        double p = 3.14;
+        //выведем число с одной цифрой после запятой
+        System.out.printf("Результат: %.1f", p);
+    }
+}
