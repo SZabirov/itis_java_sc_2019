cffe00ece045e6a90cb537a73b7db98da2e43163
Salavat Zabirov <zab.sal42@gmail.com>
2019-03-05 17:37:15 +0300

    Lesson06: files, task

--- Lesson06/src/com/company/Main06SadTask.java
+++ Lesson06/src/com/company/Main06SadTask.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Представим, что магазин был ограблен, и все продукты
  * были украдены. Но остался файл product.txt.
@@ -8,4 +12,28 @@ package com.company;
  * товаров.
  */
 public class Main06SadTask {
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("products.txt");
+        Scanner scanner = new Scanner(f);
+        int sum = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] elements = line.split(" ");
+            String strCount = elements[2];//количество в виде строки
+            String strCost = elements[3];//стоимость в виде строки
+            int count = Integer.valueOf(strCount);
+            int cost = Integer.valueOf(strCost);
+            sum = sum + count * cost;
+        }
+        System.out.println(sum);
+    }
 }
+
+
+
+
+
+
+
+
+
