15c2588828921b554b8da20e637fd0e8ed018b43
Salavat Zabirov <zab.sal42@gmail.com>
2019-03-05 18:50:25 +0300

    Lesson06: файлы, сортировки

--- Lesson06/src/com/company/Main08SelectionSortDesc.java
+++ Lesson06/src/com/company/Main08SelectionSortDesc.java
@@ -1,21 +1,23 @@
 package com.company;
 
-public class Main07SelectionSort {
+//сортировка чисел методом выбора по убыванию
+public class Main08SelectionSortDesc {
     public static void main(String[] args) {
         int[] arr = {0, 7, 1, 2, 5, 4, 7, 9, -2, -7};
         int n = arr.length;
 
         //j показывает, сколько элементов с конца стоят на своем месте
         for (int j = 0; j < n - 1; j++) {
-            int indexOfMax = 0;
+            int indexOfMin = 0;
+            //поиск минимального
             for (int i = 0; i < n - j; i++) {
-                if (arr[i] > arr[indexOfMax]) {
-                    indexOfMax = i;
+                if (arr[i] < arr[indexOfMin]) {
+                    indexOfMin = i;
                 }
             }
             //обмен
-            int b = arr[indexOfMax];
-            arr[indexOfMax] = arr[n - 1 - j];
+            int b = arr[indexOfMin];
+            arr[indexOfMin] = arr[n - 1 - j];
             arr[n - 1 - j] = b;
         }
 
---
+++ Lesson06/src/com/company/Main09HomeworkSelectionSortStrings.java
@@ -0,0 +1,10 @@
+package com.company;
+
+//отсортировать строки по возрастанию длины строки
+public class Main09HomeworkSelectionSortStrings {
+    public static void main(String[] args) {
+        String[] arr = {"aab", "bbb", "aaaa", "cc", "d"};
+
+        int x = arr[1].length();//способ узнать длину строки в ячейке массива с индексом 1
+    }
+}
