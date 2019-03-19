4a748447d164dbb27284887616c04123d5985c4d
Salavat Zabirov <zab.sal42@gmail.com>
2019-03-19 16:56:06 +0300

    Lesson10: jdbc, send query, result set example

--- lesson10/src/main/java/TestDatabase.java
+++ lesson10/src/main/java/TestDatabase.java
@@ -1,13 +1,17 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TestDatabase {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:postgresql://localhost/product_db?user=postgres&password=postgres";
         Connection c = DriverManager.getConnection(url);
         Statement s = c.createStatement();
-
+        ResultSet rs =
+                s.executeQuery("SELECT * FROM product WHERE id = 1;");
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            int cost = rs.getInt("cost");
+            System.out.println(id + " " + name + " " + cost);
+        }
     }
 }
